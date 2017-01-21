60647d9f4f42158a34596a3baaa09d72959a21f6
Kevin John <ultima301@gmail.com>
2017-01-20 23:18:39 -0800

    - Basic ship with controls - Serialized as text instead of binary

---
+++ GGJ17/Assets/Components/Controllable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour {
+
+	Engines engine;
+	void Start () {
+		engine = GetComponent<Engines>();
+	}
+	
+	void Update () {
+		var leftHeld = Input.GetKey(KeyCode.LeftArrow);
+		var rightHeld = Input.GetKey(KeyCode.RightArrow);
+		if(leftHeld && !rightHeld)
+			transform.Rotate(new Vector3(0f, 0f, engine.turning * Time.deltaTime));
+		else if(!leftHeld && rightHeld)
+			transform.Rotate(new Vector3(0f, 0f, -engine.turning * Time.deltaTime));
+
+		if(Input.GetKey(KeyCode.Space)) {
+			float angle = transform.eulerAngles.magnitude * Mathf.Deg2Rad;
+			float acceleration = engine.acceleration * Time.deltaTime;
+			Vector2 force = new Vector2(acceleration * Mathf.Cos (angle), acceleration * Mathf.Sin (angle));
+			transform.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Force);
+		}
+	}
+}
---
+++ GGJ17/Assets/Components/Engines.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Engines : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	public float acceleration = 2f;
+	public float turning = 20f;
+
+	Rigidbody2D body;
+	// Use this for initialization
+	void Start ()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		body.velocity = Vector2.ClampMagnitude(body.velocity, maxSpeed);
+	}
+}
+
