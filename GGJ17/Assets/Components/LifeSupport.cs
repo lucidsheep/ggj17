33cea55fc0f33241bdb1ae8f4327da12db12f1be
Kevin John <ultima301@gmail.com>
2017-01-21 15:48:18 -0800

    Added life support to ship

---
+++ GGJ17/Assets/Components/LifeSupport.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LifeSupport : MonoBehaviour
+{
+	public float drainPerSecond = 100f;
+
+	EnergyCore core;
+	// Use this for initialization
+	void Start ()
+	{
+		core = GetComponent<EnergyCore>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!core.UseEnergy(Mathf.FloorToInt(drainPerSecond * Time.deltaTime)))
+			SceneManager.LoadScene("gameover");
+	}
+}
+
