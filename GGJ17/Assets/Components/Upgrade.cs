5ce9beb0c23dbb4dbb5964852e4a463e2e9ed557
Kevin John <ultima301@gmail.com>
2017-01-21 16:53:25 -0800

    Added upgrades

--- GGJ17/Assets/Components/Upgrade.cs
+++ GGJ17/Assets/Components/Upgrade.cs
@@ -9,7 +9,7 @@ public class Upgrade : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	
+		GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-30f, 30f);
 	}
 	
 	// Update is called once per frame
