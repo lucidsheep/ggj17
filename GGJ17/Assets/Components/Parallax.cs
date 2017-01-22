949a2efeb223f10f44fa3a992f1c1f133f1e2d05
Kevin John <ultima301@gmail.com>
2017-01-21 22:27:56 -0800

    Pretty parallax effects!

---
+++ GGJ17/Assets/Components/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+	GameObject ship;
+
+	public float multiplier = .98f;
+
+	Vector3 startPos;
+	// Use this for initialization
+	void Start ()
+	{
+		startPos = transform.position;
+		ship = Object.FindObjectOfType<ShipSkeleton>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = ship.transform.position * multiplier + startPos;
+	}
+}
+
