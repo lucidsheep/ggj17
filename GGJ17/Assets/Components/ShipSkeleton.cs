533597b22d184437c1cb958d1bc4d6676afed188
Kevin John <ultima301@gmail.com>
2017-01-21 16:35:20 -0800

    Ship has upgrade skeletons

---
+++ GGJ17/Assets/Components/ShipSkeleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSkeleton : MonoBehaviour
+{
+	public SpriteRenderer body;
+	public SpriteRenderer guns;
+	public SpriteRenderer core;
+	public SpriteRenderer engine;
+
+	// Use this for initialization
+	void Start ()
+	{
+		guns.enabled = core.enabled = engine.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void SetShipDamage(bool damaged)
+	{
+		//todo - damaged ship sprite
+	}
+
+	public void ReceiveUpgrade(Upgrade.UpgradeType upgrade)
+	{
+		switch(upgrade)
+		{
+			case Upgrade.UpgradeType.CORE: core.enabled = true; break;
+			case Upgrade.UpgradeType.ENGINE: engine.enabled = true; break;
+			case Upgrade.UpgradeType.WEAPON: guns.enabled = true; break;
+			case Upgrade.UpgradeType.HEALTH: SetShipDamage(false); break;
+		}
+	}
+}
+
---
+++ GGJ17/Assets/Components/Upgrade.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upgrade : MonoBehaviour
+{
+	public enum UpgradeType { ENGINE, CORE, WEAPON, HEALTH }
+
+	public UpgradeType type;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.GetComponent<ShipSkeleton>() == null)
+			return;
+
+		collision.gameObject.GetComponent<ShipSkeleton>().ReceiveUpgrade(type);
+		switch(type)
+		{
+			case UpgradeType.CORE: collision.gameObject.GetComponent<EnergyCore>().LevelUp(); break;
+			case UpgradeType.ENGINE: collision.gameObject.GetComponent<Engines>().LevelUp(); break;
+			case UpgradeType.HEALTH: collision.gameObject.GetComponent<Hull>().Repair(9999); break;
+			case UpgradeType.WEAPON: break; //todo
+		}
+
+		Destroy(this.gameObject);
+	}
+}
+
