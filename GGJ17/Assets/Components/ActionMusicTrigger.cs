04935d751fc8752006b13f1cc0cddf3e8ed874e2
Kevin John <ultima301@gmail.com>
2017-01-22 14:36:24 -0800

    ActionMusicTrigger

---
+++ GGJ17/Assets/Components/ActionMusicTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMusicTrigger : MonoBehaviour
+{
+	public GameObject ship;
+
+	bool hasTriggered = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!hasTriggered && ship.transform.position.x > 250f && ship.transform.position.y > 250f){
+			hasTriggered = true;
+			AudioController.SwitchToActionTrack();
+		}
+	}
+}
+
