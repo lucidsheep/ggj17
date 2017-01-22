dadb6d32d35564bdabf924d2a2ff1ee09a66a003
Kevin John <ultima301@gmail.com>
2017-01-22 00:22:41 -0800

    Playing with more camera zoom and panning ahead of the ship's direction

--- GGJ17/Assets/Components/MCamera.cs
+++ GGJ17/Assets/Components/MCamera.cs
@@ -8,6 +8,7 @@ public class MCamera : MonoBehaviour
 	public float zoomedInMax = 5f;
 	public float zoomedOutMax = 20f;
 	public float velocityForMaxZoom = 20f;
+	public float velocityPanMultiplier = .1f;
 	// Use this for initialization
 	void Start ()
 	{
@@ -17,7 +18,10 @@ public class MCamera : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = new Vector3(thingToTrack.transform.position.x, thingToTrack.transform.position.y, transform.position.z);
+		Vector3 pos = new Vector3(thingToTrack.transform.position.x, thingToTrack.transform.position.y, transform.position.z);
+		pos += new Vector3(thingToTrack.GetComponent<Rigidbody2D>().velocity.x * velocityPanMultiplier, thingToTrack.GetComponent<Rigidbody2D>().velocity.y * velocityPanMultiplier, 0f);
+		transform.position = pos;
+		//transform.position = new Vector3(thingToTrack.transform.position.x, thingToTrack.transform.position.y, transform.position.z);
 		var velocity = thingToTrack.GetComponent<Rigidbody2D>().velocity.magnitude;
 		var targetZoom = zoomedInMax + ((velocity / velocityForMaxZoom) * (zoomedOutMax - zoomedInMax));
 		var diff = Camera.main.orthographicSize - targetZoom;
