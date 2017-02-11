1e0809e036b00f3576c3e14f1872e432a22a489b
Kevin John <ultima301@gmail.com>
2017-02-10 17:01:27 -0800

    Joystick controls work, hopefully

--- GGJ17/Assets/Components/Controllable.cs
+++ GGJ17/Assets/Components/Controllable.cs
@@ -11,21 +11,21 @@ public class Controllable : MonoBehaviour {
 	
 	void Update () {
 		GetComponent<Rigidbody2D>().angularVelocity = 0f;
-		var leftHeld = Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") < -0.25f;
-		var rightHeld = Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Horizontal") > .25f;
+		var leftHeld = Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") < -.9;
+		var rightHeld = Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Horizontal") > .9f;
 		if(leftHeld && !rightHeld)
 			engine.TurnShip(false);
 		else if(!leftHeld && rightHeld)
 			engine.TurnShip(true);
 
-		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Joystick1Button0)) {
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Joystick1Button16)) {
 			engine.EngageEngines();
-		} else if(Input.GetKeyUp(KeyCode.UpArrow))
+		} else if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.Joystick1Button16))
 		{
 			engine.StopEngines();
 		}
 
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.Joystick1Button17))
 		{
 			GetComponent<Weapons>().ShootWeapons();
 		}
